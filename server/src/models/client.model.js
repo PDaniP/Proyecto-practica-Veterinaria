aec878bbd4c20a376a190a1728b4ad305c21a0d4
Picasso017 <matiaspicasso017@gmail.com>
2026-08-28 19:51:06 -0300

    listo crud clientes

--- server/src/models/client.model.js
+++ server/src/models/client.model.js
@@ -6,4 +6,26 @@ const obtenerListaClientes = async () => {
     return rows;
 }
 
-export default obtenerListaClientes;
+const añadirClienteADB = async (cliente) => {
+    const {nombre,apellido, dni, telefono, direccion} = cliente;
+    const {rows} = await db.query('INSERT INTO clientes (nombre,apellido, dni, telefono, direccion, fecha_creacion) VALUES ($1, $2, $3, $4, $5, NOW()) RETURNING *', [nombre, apellido, dni, telefono, direccion]);
+    return rows[0];
+}
+
+const editarClienteADB = async (id, cliente) => {
+    const {nombre,apellido, dni, telefono, direccion} = cliente;
+    const {rows} = await db.query('UPDATE clientes SET nombre = $1, apellido = $2, dni = $3, telefono = $4, direccion = $5 WHERE id = $6 RETURNING *', [nombre, apellido, dni, telefono, direccion, id]);
+    return rows[0];
+}
+
+const eliminarClienteADB = async (id) => {
+    const {rows} = await db.query('DELETE FROM clientes WHERE id = $1 RETURNING *', [id]);
+    return rows[0];
+}
+
+export default {
+    obtenerListaClientes,
+    añadirClienteADB,
+    editarClienteADB,
+    eliminarClienteADB
+};
