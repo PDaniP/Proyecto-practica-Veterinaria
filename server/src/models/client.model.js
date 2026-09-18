0bc5a96be23512df563107a759932f07bf1e9705
Pablo Picasso <pablop1184@gmail.com>
2026-09-18 18:15:43 -0300

    pequeña modificacion a client.model.js, en obtenerListaClientes(). Bloqueaba las tablas

--- server/src/models/client.model.js
+++ server/src/models/client.model.js
@@ -2,7 +2,8 @@ import db from '../config/db.js';
 
 
 const obtenerListaClientes = async () => {
-    const {rows} = await db.query('SELECT * FROM clientes WHERE activo = TRUE');
+    //const {rows} = await db.query('SELECT * FROM clientes WHERE activo = TRUE'); //quite una parte porque bloqueaba las tablas
+    const { rows } = await db.query('SELECT * FROM clientes');
     return rows;
 }
 
